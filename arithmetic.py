acc121b75e2a4cb02a13ae7f1c731ae3a158e896
Aimee Galang <aimeegalang@gmail.com>
2021-07-21 03:15:24 +0000

    Final calculator-2

--- arithmetic.py
+++ arithmetic.py
@@ -3,33 +3,45 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-
-    return 10
+    return num1+num2
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-
+    return num1-num2
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    return num1*num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-
+    return num1/num2
 
 def square(num1):
     """Return the square of the input."""
-
+    return num1*num1
 
 def cube(num1):
     """Return the cube of the input."""
-
+    return num1*num1*num1
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    return num1**num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    return num1%num2
+
+def add_mult(num1, num2, num3):
+    """ Takes three numbers, adds the first two, then multiplies the sum with the third """
+    sum1 = num1 + num2
+    return sum1 * num3
+
+def add_cubes(num1, num2):
+    """ Takes two numbers, and adds together the cubes of those numbers """
+    cube1 = num1 * num1 * num1
+    cube2 = num2 * num2 * num2
+    return cube1 + cube2
+
